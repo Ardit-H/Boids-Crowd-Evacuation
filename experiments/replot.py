b5d32a6ea7cd1004c97f4375e513a63e437b18c6
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2026-07-25 00:59:20 +0200

    refactor code

--- experiments/replot.py
+++ experiments/replot.py
@@ -5,4 +5,4 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from analysis.metrics import plot_config_comparison
 
-plot_config_comparison(save_path="results/config_comparison.png")
+plot_config_comparison(save_path="../results/config_comparison.png")
